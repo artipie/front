3712374e7c2cfb3ce3f7b564795579217301da84
Yuurga <yuurga@gmail.com>
2022-02-15 17:35:46 +0300

    feat: add port argument (#7)
    Add command line argument `--port`
    Add help for cli line arguments
    
    Close: #2

--- src/main/java/com/artipie/front/Service.java
+++ src/main/java/com/artipie/front/Service.java
@@ -8,6 +8,12 @@ import com.artipie.asto.Storage;
 import com.artipie.asto.memory.InMemoryStorage;
 import com.artipie.front.internal.HealthRoute;
 import com.jcabi.log.Logger;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 /**
  * Front service.
@@ -16,17 +22,22 @@ import com.jcabi.log.Logger;
 public final class Service {
 
     /**
+     * Name for argument port for service.
+     */
+    private static final String PORT_ARG_NAME = "port";
+
+    /**
      * Configuration storage.
      */
     private final Storage storage;
 
     /**
-     * Spart service instance.
+     * Spark service instance.
      */
     private volatile spark.Service ignite;
 
     /**
-     * Service contructor.
+     * Service constructor.
      * @param storage Config storage
      */
     private Service(final Storage storage) {
@@ -35,14 +46,14 @@ public final class Service {
 
     /**
      * Start service.
+     * @param port Port for service
      */
-    private void start() {
+    private void start(final int port) {
         if (this.ignite != null) {
             throw new IllegalStateException("already started");
         }
-        // TODO: parse port from CLI args: --port=8080
-        Logger.info(this, "starting service");
-        this.ignite = spark.Service.ignite().port(8080);
+        Logger.info(this, "starting service on port: %d", port);
+        this.ignite = spark.Service.ignite().port(port);
         this.ignite.get("/.health", new HealthRoute());
         this.ignite.awaitInitialization();
         Logger.info(this, "service started on port: %d", this.ignite.port());
@@ -61,11 +72,27 @@ public final class Service {
     /**
      * Entry point.
      * @param args CLA
+     * @throws ParseException If cli line argument has wrong format
      */
-    public static void main(final String... args) {
-        // TODO: create storage from config file
+    public static void main(final String... args) throws ParseException {
+        final Options options = new Options();
+        options.addOption(
+            null,
+            Service.PORT_ARG_NAME,
+            true,
+            "service port. Should be int value. Default value 8080"
+        );
+        final CommandLineParser parser = new DefaultParser();
+        final CommandLine cmd;
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException ex) {
+            final HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("com.artipie.front.Service", options);
+            throw ex;
+        }
         final var service = new Service(new InMemoryStorage());
-        service.start();
+        service.start(Integer.parseInt(cmd.getOptionValue(Service.PORT_ARG_NAME, "8080")));
         Runtime.getRuntime().addShutdownHook(
             new Thread(() -> service.stop(), "shutdown")
         );
