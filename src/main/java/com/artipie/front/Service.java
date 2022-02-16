222f976c5900e7f8e91c6795e73ca00dcbcbf3b8
Alena <olena.gerasimova@gmail.com>
2022-02-16 12:08:37 +0300

    Create artipie setting storage (#9)
    * Create artipie setting storage
    
    * CR
    
    Co-authored-by: olenagerasimova <olena.gereasimova@gmail.com>

--- src/main/java/com/artipie/front/Service.java
+++ src/main/java/com/artipie/front/Service.java
@@ -4,14 +4,17 @@
  */
 package com.artipie.front;
 
-import com.artipie.asto.Storage;
-import com.artipie.asto.memory.InMemoryStorage;
+import com.amihaiemil.eoyaml.Yaml;
+import com.artipie.asto.blocking.BlockingStorage;
 import com.artipie.front.internal.HealthRoute;
+import com.artipie.front.settings.ArtipieYaml;
 import com.jcabi.log.Logger;
+import java.io.IOException;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
@@ -24,13 +27,21 @@ public final class Service {
     /**
      * Name for argument port for service.
      */
-    private static final String PORT_ARG_NAME = "port";
+    private static final Option PORT = new Option(
+        "p", "port", true, "service port. Should be int value. Default value 8080"
+    );
+
+    /**
+     * Name for argument config file for service.
+     */
+    private static final Option CONFIG = Option.builder().option("f").longOpt("config-file")
+        .hasArg(true).desc("The path to artipie configuration file").required(true).build();
 
     /**
      * Configuration storage.
      */
-    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-    private final Storage storage;
+    @SuppressWarnings({"PMD.SingularField", "PMD.UnusedPrivateField"})
+    private final BlockingStorage storage;
 
     /**
      * Spark service instance.
@@ -41,7 +52,7 @@ public final class Service {
      * Service constructor.
      * @param storage Config storage
      */
-    private Service(final Storage storage) {
+    private Service(final BlockingStorage storage) {
         this.storage = storage;
     }
 
@@ -50,28 +61,30 @@ public final class Service {
      * @param args CLA
      * @throws ParseException If cli line argument has wrong format
      */
+    @SuppressWarnings("PMD.DoNotCallSystemExit")
     public static void main(final String... args) throws ParseException {
         final Options options = new Options();
-        options.addOption(
-            null,
-            Service.PORT_ARG_NAME,
-            true,
-            "service port. Should be int value. Default value 8080"
-        );
+        options.addOption(Service.PORT);
+        options.addOption(Service.CONFIG);
         final CommandLineParser parser = new DefaultParser();
         final CommandLine cmd;
         try {
             cmd = parser.parse(options, args);
+            final var service = new Service(
+                new ArtipieYaml(
+                    Yaml.createYamlInput(cmd.getOptionValue(Service.CONFIG)).readYamlMapping()
+                ).storage()
+            );
+            service.start(Integer.parseInt(cmd.getOptionValue(Service.PORT, "8080")));
+            Runtime.getRuntime().addShutdownHook(new Thread(service::stop, "shutdown"));
         } catch (final ParseException ex) {
             final HelpFormatter formatter = new HelpFormatter();
             formatter.printHelp("com.artipie.front.Service", options);
             throw ex;
+        } catch (final IOException ex) {
+            Logger.error(Service.class, "Failed to read artipie setting yaml");
+            System.exit(1);
         }
-        final var service = new Service(new InMemoryStorage());
-        service.start(Integer.parseInt(cmd.getOptionValue(Service.PORT_ARG_NAME, "8080")));
-        Runtime.getRuntime().addShutdownHook(
-            new Thread(() -> service.stop(), "shutdown")
-        );
     }
 
     /**
